2fbc65202bdf5ed55720e9fd671f9cd072d75be0
mwklose <markklose17@gmail.com>
2023-04-27 14:56:00 -0400

    Added base files for Convex Hull Positivity

---
+++ src/convex_hull/positivity_by_convex_hull.py
@@ -0,0 +1,11 @@
+
+class PositivityConvexHull:
+
+    def __init__(self, treatment_data, nontreatment_data, list_of_variables):
+        # Items to hold:
+        # Original data sources, original list of variables
+        # Intersection between the two convex hulls, as polygon
+        # Intersection between the two convex hulls, as list of data
+        # List of all data not in intersection between both
+        # Ability to check additional, future data (ability to update or not)
+        pass
---
+++ src/runner_positivity.py
@@ -0,0 +1,12 @@
+# Imports
+from src.convex_hull.positivity_by_convex_hull import PositivityConvexHull
+
+if __name__ == "__main__":
+
+    # Simulation steps:
+    # 1. Generate 2D data for treatment groups.
+    # 1a. Do for complete nonpositivity
+    # 1b. Do for partial nonpositivity
+    # 1c. Do for positivity.
+    # 2. Compute Positivity Convex Hulls for the 2D data
+    print("Hello World")
